1b4d0d6fcbd4593ced69531b79ff7e849508cd51
rifqal <rifqalafiq@gmail.com>
2023-11-07 15:31:03 +0800

    second commit

---
+++ components/ProjectActions.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { deleteProject, fetchToken } from "@/lib/actions";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+type Props = {
+  projectId: string;
+};
+
+const ProjectActions = ({ projectId }: { projectId: string }) => {
+  const [isDeleting, setIsDeleting] = useState<boolean>(false);
+  const router = useRouter();
+
+  const handleDeleteProject = async () => {
+    setIsDeleting(true);
+
+    const { token } = await fetchToken();
+
+    try {
+      await deleteProject(projectId, token);
+
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsDeleting(false);
+    }
+  };
+
+  return (
+    <>
+      <Link
+        href={`/edit-project/${projectId}`}
+        className="flexCenter edit-action_btn"
+      >
+        <Image src="/pencile.svg" width={15} height={15} alt="edit" />
+      </Link>
+
+      <button
+        type="button"
+        disabled={isDeleting}
+        className={`flexCenter delete-action_btn ${
+          isDeleting ? "bg-gray" : "bg-primary-purple"
+        }`}
+        onClick={handleDeleteProject}
+      >
+        <Image src="/trash.svg" width={15} height={15} alt="delete" />
+      </button>
+    </>
+  );
+};
+
+export default ProjectActions;
